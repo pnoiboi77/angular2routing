fac28e9392729f0ded5bf8b6ccfca99003a8027f
Bernard Espanola <espanola.bernard@gmail.com>
2017-08-14 22:35:48 -0700

    componenet as component less route.

--- src/app/products/product.module.js
+++ src/app/products/product.module.js
@@ -26,26 +26,30 @@ ProductModule = __decorate([
         imports: [
             shared_module_1.SharedModule,
             router_1.RouterModule.forChild([
-                { path: 'products', component: product_list_component_1.ProductListComponent },
-                { path: 'products/:id',
-                    component: product_detail_component_1.ProductDetailComponent,
-                    resolve: {
-                        product: product_resolver_service_1.ProductResolver
-                    } },
-                { path: 'products/:id/edit',
-                    component: product_edit_component_1.ProductEditComponent,
-                    resolve: {
-                        product: product_resolver_service_1.ProductResolver
-                    },
+                { path: 'products',
                     children: [
                         { path: '',
-                            redirectTo: 'info',
-                            pathMatch: 'full' },
-                        { path: 'info',
-                            component: product_edit_info_component_1.ProductEditInfoComponent },
-                        { path: 'tags',
-                            component: product_edit_tags_component_1.ProductEditTagsComponent }
-                    ] }
+                            component: product_list_component_1.ProductListComponent },
+                        { path: ':id',
+                            component: product_detail_component_1.ProductDetailComponent,
+                            resolve: {
+                                product: product_resolver_service_1.ProductResolver
+                            } },
+                        { path: ':id/edit',
+                            component: product_edit_component_1.ProductEditComponent,
+                            resolve: {
+                                product: product_resolver_service_1.ProductResolver
+                            },
+                            children: [
+                                { path: '',
+                                    redirectTo: 'info',
+                                    pathMatch: 'full' },
+                                { path: 'info',
+                                    component: product_edit_info_component_1.ProductEditInfoComponent },
+                                { path: 'tags',
+                                    component: product_edit_tags_component_1.ProductEditTagsComponent }
+                            ] }
+                    ] },
             ])
         ],
         declarations: [
--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -14,28 +14,32 @@ import { ProductResolver } from './product-resolver.service';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  imports: [
+  imports: [ // component less route
     SharedModule,
     RouterModule.forChild([
-      { path: 'products', component: ProductListComponent },
-      { path: 'products/:id',
-        component: ProductDetailComponent,
-        resolve: {
-          product: ProductResolver } },
-      { path: 'products/:id/edit',
-        component: ProductEditComponent,
-        resolve: {
-          product: ProductResolver
-          /*product: 'productResolverInline'*/ },
+      { path: 'products', 
         children: [
           { path: '',
-            redirectTo: 'info',
-            pathMatch: 'full' },
-          { path: 'info',
-              component: ProductEditInfoComponent },
-          { path: 'tags',
-              component: ProductEditTagsComponent }
-        ] }
+            component: ProductListComponent },
+          { path: ':id',
+            component: ProductDetailComponent,
+            resolve: {
+              product: ProductResolver } },
+          { path: ':id/edit',
+            component: ProductEditComponent,
+            resolve: {
+              product: ProductResolver
+              /*product: 'productResolverInline'*/ },
+            children: [
+              { path: '',
+                redirectTo: 'info',
+                pathMatch: 'full' },
+              { path: 'info',
+                  component: ProductEditInfoComponent },
+              { path: 'tags',
+                  component: ProductEditTagsComponent }
+            ] }
+        ] },
     ])
   ],
   declarations: [
